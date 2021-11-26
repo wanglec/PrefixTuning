1c4f8b4c24e0da2fa5ce5e16d869e014e590ec0c
wanglechuan-gif <wanglechuan@evocolabs.com>
2021-11-25 23:11:27 -0800

    update run.py

--- run.py
+++ run.py
@@ -21,8 +21,8 @@ def main(targets):
     if 'test' in targets:
         os.chdir("gpt2/webnlg_models/webnlgprefixtune_y_5_act_cat_b=5-e=5_d=0.0_u=no_lr=5e-05_w=0.0_s=10_r=n_m=512_o=1_o=1")
         files = [f for f in os.listdir('.') if os.path.isfile(f)]
-        if 'pytorch_model.bin' not in files:
-            print('Since the model parameters have large size (>100 Mb), it could not be uploaded to github. We are figuring out a way to share the model with you.')
+        if 'pytorch_model.bin'  in files:
+            print('Since the model parameters have large size (>100 Mb), it could not be uploaded to github. We are figuring out a way to share the model with you. Some sample outputs generated from the model are in path `PrefixTuning/gpt2/res/webNLG_results2.`')
         else:
             COMMANDLINE = "python gen.py webnlg yes test webnlg_models/webnlgprefixtune_y_5_act_cat_b=5-e=5_d=0.0_u=no_lr=5e-05_w=0.0_s=10_r=n_m=512_o=1_o=1 no"
             os.chdir("../../../gpt2/")
